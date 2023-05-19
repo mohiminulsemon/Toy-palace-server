797935b1000bd7e4dc1e97b3d2833d2f1d77bddf
mohiminulsemon <mohiminulsemon80@gmail.com>
2023-05-19 11:02:02 +0600

    added delete option

--- index.js
+++ index.js
@@ -67,7 +67,17 @@ async function run() {
     });
 
 
-    
+    app.delete('/toys/:id', async(req,res) => {
+      const id = req.params.id;
+     
+      const filter = {_id : new ObjectId(id)}
+ 
+  
+      const result= await toyCollection.deleteOne(filter)
+      res.send(result)
+
+    });
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
